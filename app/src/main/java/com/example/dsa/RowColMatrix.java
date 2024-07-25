51cf0b32da355f045c59753363719574e0c24f85
Sajid Ali Suthar <sutharsajidali@gmail.com>
2024-07-25 11:36:22 +0530

    Search element in matrix 2D array

---
+++ app/src/main/java/com/example/dsa/RowColMatrix.java
@@ -0,0 +1,33 @@
+package com.example.dsa;
+
+import java.util.Arrays;
+
+public class RowColMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,39},
+                {33,34,38,50}
+        };
+        int target = 25;
+        System.out.println(Arrays.toString(search(matrix, target)));
+    }
+    static int[] search(int[][] matrix, int target){
+        int row = 0;
+        int column = matrix.length-1;
+
+        while (row < matrix.length && column >=0){
+            if (matrix[row][column] == target){
+                return new int[] {row, column};
+            }
+            if (matrix[row][column] < target){
+                row++;
+            }else {
+                column--;
+            }
+        }
+
+        return new int[] {-1, -1};
+    }
+}
