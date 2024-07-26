2a85bfbf2480ad5c66a8848383650ddc0771e769
Sajid Ali Suthar <sutharsajidali@gmail.com>
2024-07-26 12:04:03 +0530

    Swapped check added

--- app/src/main/java/com/example/dsa/BubbleSort.java
+++ app/src/main/java/com/example/dsa/BubbleSort.java
@@ -10,14 +10,21 @@ public class BubbleSort {
     }
 
     static void bubbleSort(int[] arr){
+        boolean swapped;
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j]<arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
                 }
+                swapped = true;
+            }
+            if (!swapped){
+                break;
             }
         }
+
     }
 }
